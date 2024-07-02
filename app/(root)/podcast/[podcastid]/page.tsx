544949714b24d90fbf6835d751efe20188fd59db
Nikita Demianov <hourlystory@gmail.com>
2024-07-02 15:53:33 +0300

    Add file structude and left sidebar

---
+++ app/(root)/podcast/[podcastid]/page.tsx
@@ -0,0 +1,12 @@
+import { Podcast } from 'lucide-react'
+import React from 'react'
+
+const PodcastDetails = ({ params }: {params: {
+    podcastId: string
+}}) => {
+  return (
+    <p className='text-white-1'>PodcastDetails for {params.podcastId}</p>
+  )
+}
+
+export default PodcastDetails
